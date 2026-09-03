76b63da3f62dc7901a851065a3da61d0e4282e8e
bfmounir <bfmounir@gmail.com>
2026-09-03 13:45:31 +0200

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "ضع_هنا_API Key";
+const SUPABASE_URL = "ضع_هنا_https://adhjgndvffctdxnxvdnv.supabase.co";
 const SUPABASE_KEY = "ضع_هنا_sb_publishable_tCQP8XMTHijRBnzkp8i2OA__jPUX_Oe";
